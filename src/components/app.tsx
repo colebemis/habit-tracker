97837f013c2eae876fbfda1eca2b9a47a1de4385
Cole Bemis <colebemis@github.com>
2023-02-24 00:32:44 -0700

    Refactor

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -36,51 +36,59 @@ export function App() {
 
   const endDate = today;
 
-  const months = React.useMemo(() => {
-    // Create an array of dates in the range
-    const dates = eachDayOfInterval({ start: startDate, end: endDate });
-
-    // Group the dates by month
-    return Object.entries(
-      groupBy(dates, (date) => {
-        const year = date.getFullYear();
-        const month = date.getMonth();
-        return `${year}-${month}`;
-      })
-    );
+  // Create an array of dates in the range
+  const dates = React.useMemo(() => {
+    return eachDayOfInterval({ start: startDate, end: endDate });
   }, [startDate]);
+
+  // Track which dates are in view
+  const [datesInView, setDatesInView] = React.useState(
+    () => new Set<number>(dates.map((date) => date.valueOf()))
+  );
+
+  const minDateInView = React.useMemo(() => {
+    return new Date(Math.min(...datesInView));
+  }, [datesInView]);
+
+  const handleIntersectionChange = React.useCallback(
+    (event: { date: Date; inView: boolean }) => {
+      // Update set of dates in view
+      setDatesInView((prev) => {
+        const next = new Set(prev);
+        if (event.inView) {
+          next.add(event.date.valueOf());
+        } else {
+          next.delete(event.date.valueOf());
+        }
+        return next;
+      });
+
+      // Increase range when start comes into view
+      if (event.inView && event.date.valueOf() === startDate.valueOf()) {
+        setStartDate(addMonths(startDate, -1));
+      }
+    },
+    [startDate]
+  );
 
   return (
     <div className="flex h-screen w-screen flex-row-reverse overflow-auto supports-[height:100svh]:h-[100svh]">
       <div>
         <div className="table border-b border-divider">
-          <div className="flex">
-            {months.map(([key, dates]) => {
-              const [year, month] = key.split("-").map(Number);
-              return (
-                <MonthHeader
-                  key={key}
-                  year={year}
-                  month={month}
-                  dates={dates}
-                  onEnterView={() => {
-                    const previousMonth = addMonths(
-                      new Date(year, month, 1),
-                      -1
-                    );
-
-                    if (previousMonth < startDate) {
-                      setStartDate(previousMonth);
-                    }
-                  }}
-                  onExitView={(to) => {
-                    if (to === "left") {
-                      setStartDate(dates[0]);
-                    }
-                  }}
+          <div className="px-4 py-2">
+            <h2 className="sticky left-4 inline-block py-2 text-3xl font-semibold leading-8">
+              {MONTH_NAMES[minDateInView.getMonth()]}{" "}
+              {minDateInView.getFullYear()}
+            </h2>
+            <div className="flex gap-1">
+              {dates.map((date) => (
+                <CalendarDate
+                  key={date.valueOf()}
+                  date={date}
+                  onIntersectionChange={handleIntersectionChange}
                 />
-              );
-            })}
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -88,64 +96,26 @@ export function App() {
   );
 }
 
-function MonthHeader(props: {
-  year: number;
-  month: number;
-  dates: Date[];
-  onEnterView?: () => void;
-  onExitView?: (to: "left" | "right") => void;
-}) {
-  const [previouslyInView, setPreviouslyInView] = React.useState(false);
-  const { ref } = useInView({
-    onChange: (inView, entry) => {
-      console.log(entry);
+const CalendarDate = React.memo(
+  (props: {
+    date: Date;
+    onIntersectionChange?: (event: { date: Date; inView: boolean }) => void;
+  }) => {
+    const { ref } = useInView({
+      onChange: (inView) => {
+        props.onIntersectionChange?.({ date: props.date, inView });
+      },
+      threshold: 1,
+    });
 
-      if (!previouslyInView && inView) {
-        props.onEnterView?.();
-        setPreviouslyInView(true);
-      }
-
-      if (previouslyInView && !inView) {
-        props.onExitView?.(entry.boundingClientRect.x > 0 ? "right" : "left");
-        setPreviouslyInView(false);
-      }
-    },
-  });
-
-  return (
-    <div className="px-4 py-2">
-      <h2
+    return (
+      <div
         ref={ref}
-        className="sticky left-4 inline-block py-2 text-3xl font-semibold leading-8"
+        className="flex w-12 flex-col items-center gap-1 py-2 text-sm leading-4 text-text-secondary"
       >
-        {MONTH_NAMES[props.month]} {props.year}
-      </h2>
-      <div className="flex gap-1">
-        {props.dates.map((date) => (
-          <div
-            key={date.valueOf()}
-            className="flex w-12 flex-col items-center gap-1 py-2 text-sm leading-4 text-text-secondary"
-          >
-            <span>{DAY_NAMES[date.getDay()].slice(0, 1)}</span>
-            <span>{date.getDate()}</span>
-          </div>
-        ))}
+        <span>{DAY_NAMES[props.date.getDay()].slice(0, 1)}</span>
+        <span>{props.date.getDate()}</span>
       </div>
-    </div>
-  );
-}
-
-/** Group an array of items by a key. */
-function groupBy<T, K extends string | number>(
-  list: T[],
-  keyGetter: (item: T) => K
-): Record<K, T[]> {
-  return list.reduce((acc, item) => {
-    const key = keyGetter(item);
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push(item);
-    return acc;
-  }, {} as Record<K, T[]>);
-}
+    );
+  }
+);
